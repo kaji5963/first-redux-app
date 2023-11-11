dbdaecc1baf1b63f90492d27fb026e77984a8a61
takuya_kajiwara <fd.elt.119@gmail.com>
2023-11-11 19:08:22 +0900

    リファクタリング、reducer作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // extension追加
+);
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,41 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-
-// actions　〜　increment, decrement
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  };
-};
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  };
-};
-
-// reducer
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT': {
-      return state + 1;
-    }
-    case 'DECREMENT': {
-      return state - 1;
-    }
-  }
-};
-
-// store
-let store = createStore(counterReducer);
-
-// consoleに新しい状態を出力
-store.subscribe(() => console.log(store.getState()));
-
-// dispatch
-store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
